237f619b49e4b72e8ce0229f62a87a1aa6158926
Thomas Cherret <b00705488@essec.edu>
2023-01-03 15:10:05 +0100

    automatic push

--- src/connectors/lido/V1-polygon/pools.js
+++ src/connectors/lido/V1-polygon/pools.js
@@ -1,16 +1,16 @@
 const POOLS = [
-  // {
-  //   name: 'Matic Liquid Pool',
-  //   chain: 'polygon',
-  //   underlying_tokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'],
-  //   pool_address: '0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4',
-  //   investing_address: '0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4',
-  //   staking_address: null,
-  //   boosting_address: null,
-  //   distributor_address: null,
-  //   rewards_tokens: null,
-  //   metadata: {},
-  // },
+  {
+    name: 'Matic Liquid Pool',
+    chain: 'ethereum',
+    underlying_tokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'],
+    pool_address: '0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4',
+    investing_address: '0x3a58a54c066fdc0f2d55fc9c89f0415c92ebf3c4',
+    staking_address: null,
+    boosting_address: null,
+    distributor_address: null,
+    rewards_tokens: null,
+    metadata: {},
+  },
 ];
 
 /// pools
