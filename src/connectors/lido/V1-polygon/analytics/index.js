b6cb9ceac31027709f8bfee2933a266922408755
Thomas Cherret <b00705488@essec.edu>
2023-01-03 18:42:50 +0100

    automatic push

--- src/connectors/lido/V1-polygon/analytics/index.js
+++ src/connectors/lido/V1-polygon/analytics/index.js
@@ -4,17 +4,20 @@ const external = require('./external/DefiLlama/index');
 const pools = require('../pools');
 const checkLidoPolygonV1APY = require('./functions/apyActivity');
 const checkLidoPolygonV1TVL = require('./functions/tvl');
+const checkLidoPolygonV1Shareprice = require('./functions/sharePrice');
 
 async function analytics(chain, poolAddress) {
   const apy = await checkLidoPolygonV1APY(chain, poolAddress);
   if (apy.err) throw new Error(apy.err);
-  const activity_apy = parsefLoat(String(apy.data));
+  const activity_apy = parseFLoat(String(apy.data));
   const TVL = await checkLidoPolygonV1TVL(chain, poolAddress);
   if (TVL.err) throw new Error(TVL.err);
+  const shareprice = await checkLidoPolygonV1Shareprice(chain, poolAddress);
+  if (shareprice.err) throw new Error(shareprice.err);
 
   const result = {
     status: null,
-    tvl: tvl,
+    tvl: TVL.data,
     liquidity: 0,
     outloans: null,
     losses: null,
@@ -22,13 +25,11 @@ async function analytics(chain, poolAddress) {
     apy: activity_apy,
     activity_apy: activity_apy,
     rewards_apy: 0,
-    boosting_apy: null,
-    share_price: null,
+    boosting_apy: 0,
+    share_price: shareprice.data,
     minimum_deposit: null,
     maximum_deposit: null,
   };
-
-  console.log(result);
 
   return result;
 }
