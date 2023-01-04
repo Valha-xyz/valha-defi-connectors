42ee3e7114cacf8eb1cd6ae28d741199472b16ad
Thomas Cherret <b00705488@essec.edu>
2023-01-04 20:10:03 +0100

    automatic push

--- src/connectors/pancakeswap/v2/analytics/index.js
+++ src/connectors/pancakeswap/v2/analytics/index.js
@@ -1,18 +1,41 @@
+const external = require('./external/DefiLlama/index');
+
+/// APY
+/// TVL
+async function loadExternal() {
+  const pools = await external.apy();
+  if (!pools || pools.length === 0) {
+    return null;
+  }
+  return pools;
+}
+
 async function analytics(chain, poolAddress) {
-  const tvl = 0;
+  const POOLS = await pools();
+  if (!POOLS || POOLS.length === 0) return {};
+  const externalInformation = await loadExternal();
+  if (!externalInformation) return {};
+  const externalInfo = _.find(externalInformation, (elem) => {
+    return elem.pool.includes(poolAddress.toLowerCase());
+  });
+
+  const tvl = externalInfo['tvlUsd'];
+  const rewardsAPY = externalInfo['apyReward'];
+  const activityAPY = externalInfo['apyBase'];
+  const totalAPY = rewardsAPY + activityAPY;
 
   const result = {
-    status: null,
-    tvl: 10,
-    liquidity: 10,
-    outloans: 10,
+    status: true,
+    tvl: tvl,
+    liquidity: tvl,
+    outloans: null,
     losses: null,
-    capacity: 5,
-    apy: 5.5,
-    activity_apy: 3,
-    rewards_apy: 2.5,
+    capacity: Number.MAX_SAFE_INTEGER,
+    apy: totalAPY,
+    activity_apy: activityAPY ? activityAPY : 0,
+    rewards_apy: rewardsAPY ? rewardsAPY : 0,
     boosting_apy: 0,
-    share_price: 1,
+    share_price: null,
     minimum_deposit: null,
     maximum_deposit: null,
   };
