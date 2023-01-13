0a1b1a5f95816f8988cac67f7bd846e33be25e27
Thomas Cherret <b00705488@essec.edu>
2023-01-13 10:44:43 +0100

    automatic push

--- src/connectors/rocketpool/v0/interactions/index.js
+++ src/connectors/rocketpool/v0/interactions/index.js
@@ -43,45 +43,46 @@ async function deposit(
   };
 }
 
-/// redeem
-async function redeem(
-  pool_name,
-  chain,
-  underlying_tokens,
-  pool_address,
-  investing_address,
-  staking_address,
-  boosting_address,
-  distributor_address,
-  rewards_tokens,
-  metadata,
-  amountNotBN,
-  amountsDesiredNotBN,
-  amountsMinimumNotBN,
-  ranges,
-  rangeToken,
-  userAddress,
-  receiverAddress,
-  lockupTimestamp,
-  deadline
-) {
-  const abi = PoolABI;
-  const method_name = 'withdrawExcessBalance';
-  const position_token = pool_address;
-  const amountBN = await toBnERC20Decimals(amountNotBN, chain, position_token);
-  const args = [amountBN];
-  const interaction_address = investing_address;
+//// NOT A REAL "REDEEM" function, it enables to get the balance that was not staked yet but send to RocketPool
+// /// redeem
+// async function redeem(
+//   pool_name,
+//   chain,
+//   underlying_tokens,
+//   pool_address,
+//   investing_address,
+//   staking_address,
+//   boosting_address,
+//   distributor_address,
+//   rewards_tokens,
+//   metadata,
+//   amountNotBN,
+//   amountsDesiredNotBN,
+//   amountsMinimumNotBN,
+//   ranges,
+//   rangeToken,
+//   userAddress,
+//   receiverAddress,
+//   lockupTimestamp,
+//   deadline
+// ) {
+//   const abi = PoolABI;
+//   const method_name = 'withdrawExcessBalance';
+//   const position_token = pool_address;
+//   const amountBN = await toBnERC20Decimals(amountNotBN, chain, position_token);
+//   const args = [amountBN];
+//   const interaction_address = investing_address;
 
-  return {
-    abi: abi, //json file name
-    method_name: method_name, //method to interact with the pool
-    position_token: position_token, // token needed to approve
-    position_token_type: 'ERC-20', //token type to approve
-    interaction_address: interaction_address, // contract to interact with to interact with poolAddress
-    amount: amountBN, //amount that will be use in the ERC20 approve tx of the position token is an ERC20 or that will be use as the 'value' of the transaction
-    args: args, //args to pass to the smart contracts to trigger 'method_name'
-  };
-}
+//   return {
+//     abi: abi, //json file name
+//     method_name: method_name, //method to interact with the pool
+//     position_token: position_token, // token needed to approve
+//     position_token_type: 'ERC-20', //token type to approve
+//     interaction_address: interaction_address, // contract to interact with to interact with poolAddress
+//     amount: amountBN, //amount that will be use in the ERC20 approve tx of the position token is an ERC20 or that will be use as the 'value' of the transaction
+//     args: args, //args to pass to the smart contracts to trigger 'method_name'
+//   };
+// }
 
 // /// stake
 // async function stake(
@@ -183,7 +184,7 @@ module.exports = {
   deposit: deposit,
   deposit_and_stake: null,
   unlock: null,
-  redeem: redeem,
+  redeem: null,
   stake: null,
   unstake: null,
   boost: null,
