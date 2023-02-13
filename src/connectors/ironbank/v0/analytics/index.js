2221d5ccedb4b3d87a33803526b4f3cacede5ad9
Thomas Cherret <b00705488@essec.edu>
2023-02-13 14:17:54 +0100

    automatic push

--- src/connectors/ironbank/v0/analytics/index.js
+++ src/connectors/ironbank/v0/analytics/index.js
@@ -1,26 +1,44 @@
-async function analytics (chain, poolAddress) {
-  const tvl = 0
+const _ = require('lodash');
+const checkIBData = require('./function/getIBData');
 
-  const result = {
-    status: null,
-    tvl: 10,
-    liquidity: 10,
-    outloans: 10,
-    losses: null,
-    capacity: 5,
-    apy: 5.5,
-    activity_apy: 3,
-    rewards_apy: 2.5,
-    boosting_apy: 0,
-    share_price: 1,
-    minimum_deposit: null,
-    maximum_deposit: null
-  }
+async function analytics(chain, poolAddress) {
+  const info = await checkIBData(chain, poolAddress);
+  console.log(info);
 
-  return result
+  const total
+  const TVL = info['totalSupplyUsd'] ? info['totalSupplyUsd'] : 0;
+  // const outloans = info['totalBorrowsUsd'] ? info['totalBorrowsUsd'] : 0;
+  // const liquidity = TVL - outloans;
+  // const sharePrice = info['exchangeRateHumanReadable']
+  //   ? info['exchangeRateHumanReadable']
+  //   : 0;
+  // const ActAPY = info['supplyApy'] ? info['supplyApy'] : 0;
+  // const RewAPY = 0;
+  // const totalAPY = ActAPY + RewAPY;
+
+  // const result = {
+  //   status: true,
+  //   tvl: TVL,
+  //   liquidity: liquidity,
+  //   outloans: outloans,
+  //   losses: null,
+  //   capacity: Number.MAX_SAFE_INTEGER,
+  //   apy: totalAPY,
+  //   activity_apy: ActAPY,
+  //   rewards_apy: RewAPY,
+  //   boosting_apy: null,
+  //   share_price: sharePrice,
+  //   minimum_deposit: null,
+  //   maximum_deposit: null,
+  // };
+
+  // console.log(result);
+  // return result;
 }
 
-module.exports = {
-  main: analytics,
-  url: external.url
-}
+// module.exports = {
+//   main: analytics,
+//   url: 'https://fluxfinance.com/',
+// };
+
+analytics('optimism', '0x04F0fd3CD03B17a3E5921c0170ca6dD3952841cA');
