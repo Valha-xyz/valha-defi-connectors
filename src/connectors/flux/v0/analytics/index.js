a57b4aaa998f3b716ee5d4d319fddc993d5850bd
Thomas Cherret <b00705488@essec.edu>
2023-02-13 11:53:33 +0100

    automatic push

--- src/connectors/flux/v0/analytics/index.js
+++ src/connectors/flux/v0/analytics/index.js
@@ -1,26 +1,41 @@
-async function analytics (chain, poolAddress) {
-  const tvl = 0
+/* eslint-disable @typescript-eslint/no-var-requires */
+const _ = require('lodash');
+const checkFluxData = require('./functions/getData');
+
+async function analytics(chain, poolAddress) {
+  const info = await checkFluxData(chain, poolAddress);
+
+  const TVL = info['totalSupplyUsd'] ? info['totalSupplyUsd'] : 0;
+  const outloans = info['totalBorrowsUsd'] ? info['totalBorrowsUsd'] : 0;
+  const liquidity = TVL - outloans;
+  const sharePrice = info['exchangeRateHumanReadable']
+    ? info['exchangeRateHumanReadable']
+    : 0;
+  const ActAPY = info['supplyApy'] ? info['supplyApy'] : 0;
+  const RewAPY = 0;
+  const totalAPY = ActAPY + RewAPY;
 
   const result = {
-    status: null,
-    tvl: 10,
-    liquidity: 10,
-    outloans: 10,
+    status: true,
+    tvl: TVL,
+    liquidity: liquidity,
+    outloans: outloans,
     losses: null,
-    capacity: 5,
-    apy: 5.5,
-    activity_apy: 3,
-    rewards_apy: 2.5,
-    boosting_apy: 0,
-    share_price: 1,
+    capacity: Number.MAX_SAFE_INTEGER,
+    apy: totalAPY,
+    activity_apy: ActAPY,
+    rewards_apy: RewAPY,
+    boosting_apy: null,
+    share_price: sharePrice,
     minimum_deposit: null,
-    maximum_deposit: null
-  }
+    maximum_deposit: null,
+  };
 
-  return result
+  console.log(result);
+  return result;
 }
 
 module.exports = {
   main: analytics,
-  url: external.url
-}
+  url: 'https://fluxfinance.com/',
+};
