83314d3732b2bc17c76f22808eb784317c9ba16f
Thomas Cherret <b00705488@essec.edu>
2023-02-15 18:01:42 +0100

    automatic push

---
+++ src/connectors/convex/arbitrum-v0/analytics/functions/getData.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const axios = require('axios');
+
+async function checkConvexData(chain, poolAddress, id) {
+  try {
+    let url = '';
+    let poolId = '';
+    if (chain === 'ethereum') {
+      url = 'https://www.convexfinance.com/api/curve-apys';
+      pooLId = id;
+    } else if (chain === 'arbitrum') {
+      url = 'https://www.convexfinance.com/api/sidechains-apys';
+      poolId = 'arbitrum-' + poolAddress.toLowerCase();
+    } else {
+      throw new Error(
+        'Error: error with Convex Indexer for APYs. Unknown chain.'
+      );
+    }
+    const { data } = await axios.get(url);
+    if (!data || !data.apys) {
+      throw new Error(`Data from Flux indexer not ok for ${poolAddress}`);
+    }
+    let info = data.apys;
+    const result = info[poolId];
+    return { data: result, err: null };
+  } catch (err) {
+    console.log(err);
+    return { data: null, err };
+  }
+}
+
+module.exports = checkConvexData;
---
+++ src/connectors/convex/arbitrum-v0/analytics/index.js
@@ -0,0 +1,71 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const _ = require('lodash');
+const pools = require('../pools/pools');
+const getCurvePoolTVL = require('./functions/getCurvePoolTvl');
+const checkConvexData = require('./functions/getData');
+const checkPoolSupply = require('./functions/totalSupply');
+
+async function analytics(chain, poolAddress) {
+  // Find pool information
+  const POOLS = await pools();
+  if (!POOLS || POOLS.length === 0) return {};
+  const poolInfo = _.find(POOLS, (elem) => {
+    return elem.pool_address.toLowerCase() === poolAddress.toLowerCase();
+  });
+
+  // Find TVL information
+  const underlyingLPAddress =
+    poolInfo.underlying_tokens.length > 0
+      ? poolInfo.underlying_tokens[0]
+      : null;
+  if (!underlyingLPAddress) {
+    throw new Error(
+      `Error: impossible to find the LP pool on Cvx for ${poolAddress}`
+    );
+  }
+  const supplyCvxInfo = await checkPoolSupply(chain, poolAddress);
+  if (supplyCvxInfo.err) throw new Error(supplyCvxInfo.err.message);
+  const supplyCvx = supplyCvxInfo.data;
+  const supplyCrvInfo = await checkPoolSupply(chain, underlyingLPAddress);
+  if (supplyCrvInfo.err) throw new Error(supplyCrvInfo.err.message);
+  const supplyCrv = supplyCrvInfo.data;
+  const CrvInfo = await getCurvePoolTVL(chain);
+  if (CrvInfo.err) throw new Error(CrvInfo.err.message);
+  const tvlUSDCrv = CrvInfo.data.tvlUsd;
+  const TVL = (supplyCvx / supplyCrv) * tvlUSDCrv;
+  const liquidity = TVL;
+  const sharePrice = 1;
+
+  // Find APYs information
+  const id = CrvInfo.data.id;
+  const info = await checkConvexData(chain, poolAddress, id);
+  if (info.err) throw new Error(info.err.message);
+  const apyInfo = info.data;
+  const ActAPY = apyInfo ? apyInfo['baseApy'] : 0;
+  const RewAPY = apyInfo ? 0 : 0;
+  const totalAPY = ActAPY + RewAPY;
+
+  const result = {
+    status: true,
+    tvl: TVL,
+    liquidity: liquidity,
+    outloans: null,
+    losses: null,
+    capacity: tvlUSDCrv, // tvl of underlying
+    apy: totalAPY,
+    activity_apy: ActAPY,
+    rewards_apy: RewAPY,
+    boosting_apy: null,
+    share_price: sharePrice,
+    minimum_deposit: null,
+    maximum_deposit: null,
+  };
+
+  console.log(result);
+  return result;
+}
+
+module.exports = {
+  main: analytics,
+  url: 'https://www.convexfinance.com/stake',
+};
