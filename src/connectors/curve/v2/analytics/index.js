305a1706a8f01c1560c1ac88806965746ddcdd4d
Thomas Cherret <b00705488@essec.edu>
2023-02-15 23:55:19 +0100

    automatic push

--- src/connectors/curve/v2/analytics/index.js
+++ src/connectors/curve/v2/analytics/index.js
@@ -1,21 +1,39 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const _ = require('lodash');
+const external = require('./external/DefiLlama/index');
+const pools = require('../pools/pools');
+const checkSharePrice = require('./functions/sharePrice');
+const checkAngleV1APYTVL = require('./functions/apy');
+
 async function analytics(chain, poolAddress) {
-  const tvl = 0;
+  const POOLS = await pools();
+  if (!POOLS || POOLS.length === 0) return {};
+  const poolInfo = _.find(POOLS, (elem) => {
+    return elem.pool_address.toLowerCase() === poolAddress.toLowerCase();
+  });
+
+  const ActAPY = dataAPY ? dataAPY.activity_apy : 0;
+  const RewAPY = dataAPY ? dataAPY.rewards_apy : 0;
+  const totalAPY = ActAPY + RewAPY;
+  const TVL = dataAPY ? dataAPY.tvl : 0;
 
   const result = {
     status: null,
-    tvl: 10,
-    liquidity: 10,
-    outloans: 10,
+    tvl: TVL,
+    liquidity: TVL,
+    outloans: null,
     losses: null,
-    capacity: 5,
-    apy: 5.5,
-    activity_apy: 3,
-    rewards_apy: 2.5,
-    boosting_apy: 0,
-    share_price: 1,
+    capacity: Number.MAX_SAFE_INTEGER,
+    apy: totalAPY,
+    activity_apy: ActAPY,
+    rewards_apy: RewAPY,
+    boosting_apy: null,
+    share_price: sharePrice.data,
     minimum_deposit: null,
     maximum_deposit: null,
   };
+
+  console.log(result);
 
   return result;
 }
---
+++ src/connectors/curve/v2/interactions/index.ts
@@ -0,0 +1,133 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import {
+  AdditionalOptions,
+  AddressesInput,
+  AmountInput,
+  Interactions,
+  InteractionsReturnObject,
+  Pool,
+} from '../../../../utils/types/connector-types';
+const ethers = require('ethers');
+const { getNodeProvider } = require('../../../../utils/getNodeProvider');
+const { toBnERC20Decimals } = require('../../../../utils/toBNTokenDecimals');
+import { StableABI2 } from './../abi/Stable2';
+import { StableABI3 } from './../abi/Stable3';
+import { StableABI4 } from './../abi/Stable4';
+import { GaugeABI } from '../abi/Gauge';
+
+/// invest
+async function deposit(
+  pool: Pool,
+  amount: AmountInput,
+  addresses: AddressesInput,
+  options?: AdditionalOptions
+): Promise<InteractionsReturnObject> {
+  const size = pool.underlying_tokens.length;
+  let abi: any;
+  if (size === 2) {
+    abi = StableABI2;
+  } else if (size === 3) {
+    abi = StableABI3;
+  } else if (size === 4) {
+    abi = StableABI4;
+  } else {
+    throw new Error('Error: pool size is not handle.');
+  }
+  const method_name = `add_liquidity(uint256[${size}], uint256)`;
+  const position_token = pool.underlying_tokens;
+  const amountsBN = [];
+  for (const i in pool.underlying_tokens) {
+    const amountBN = await toBnERC20Decimals(
+      amount.amountsDesired[i],
+      pool.chain,
+      pool.underlying_tokens[i]
+    );
+    amountsBN.push(amountBN);
+  }
+  const amountMinimum = await toBnERC20Decimals(
+    amount.amountsMinimum[0],
+    pool.chain,
+    pool.pool_address
+  );
+  const args = [amountsBN, amountMinimum];
+
+  return {
+    txInfo: {
+      abi: abi, //abi array
+      interaction_address: pool.investing_address, // contract to interact with to interact with poolAddress
+      method_name: method_name, //method to interact with the pool
+      args: args, //args to pass to the smart contracts to trigger 'method_name'
+    },
+    assetInfo: {
+      position_token: position_token, // token needed to approve
+      position_token_type: 'ERC-20', //token type to approve
+      amount: amountsBN,
+    },
+  };
+}
+
+/// invest
+async function redeem(
+  pool: Pool,
+  amount: AmountInput,
+  addresses: AddressesInput,
+  options?: AdditionalOptions
+): Promise<InteractionsReturnObject> {
+  const size = pool.underlying_tokens.length;
+  let abi: any;
+  if (size === 2) {
+    abi = StableABI2;
+  } else if (size === 3) {
+    abi = StableABI3;
+  } else if (size === 4) {
+    abi = StableABI4;
+  } else {
+    throw new Error('Error: pool size is not handle.');
+  }
+  const method_name = `remove_liquidity(uint256, uint256[${size}])`;
+  const position_token = pool.pool_address;
+  const amountsBN = [];
+  for (const i in pool.underlying_tokens) {
+    const amountBN = await toBnERC20Decimals(
+      amount.amountsMinimum[i],
+      pool.chain,
+      pool.underlying_tokens[i]
+    );
+    amountsBN.push(amountBN);
+  }
+  const amountDesired = await toBnERC20Decimals(
+    amount.amountsMinimum[0],
+    pool.chain,
+    position_token
+  );
+  const args = [amountDesired, amountsBN];
+
+  return {
+    txInfo: {
+      abi: abi, //abi array
+      interaction_address: pool.investing_address, // contract to interact with to interact with poolAddress
+      method_name: method_name, //method to interact with the pool
+      args: args, //args to pass to the smart contracts to trigger 'method_name'
+    },
+    assetInfo: {
+      position_token: position_token, // token needed to approve
+      position_token_type: 'ERC-20', //token type to approve
+      amount: amountDesired,
+    },
+  };
+}
+
+const interactions: Interactions = {
+  deposit: deposit,
+  deposit_and_stake: null,
+  unlock: null,
+  redeem: redeem,
+  stake: stake,
+  unstake: unstake,
+  boost: null,
+  unboost: null,
+  claim_rewards: claimRewards,
+  claim_interests: null,
+};
+
+export default interactions;
