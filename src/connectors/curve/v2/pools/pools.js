0ebe6867153ffb5a2a1b82155ea330cddaa51551
Thomas Cherret <b00705488@essec.edu>
2023-02-15 23:04:12 +0100

    automatic push

--- src/connectors/curve/v2/pools/pools.js
+++ src/connectors/curve/v2/pools/pools.js
@@ -1,21 +1,9 @@
-const POOLS = [
-  {
-    name: 'Pool Name',
-    chain: 'ethereum',
-    underlying_tokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
-    pool_address: '0x0000000000000000000000000000000000000000',
-    investing_address: '0x0000000000000000000000000000000000000000',
-    staking_address: '0x0000000000000000000000000000000000000000',
-    boosting_address: null,
-    distributor_address: null,
-    rewards_tokens: ['0x0000000000000000000000000000000000000000'],
-    metadata: {}
-  }
-]
+/* eslint-disable @typescript-eslint/no-var-requires */
+const POOLS = require('./generatedPools.json');
 
 /// pools
-async function pools () {
-  return POOLS
+async function pools() {
+  return POOLS;
 }
 
-module.exports = pools
+module.exports = pools;
