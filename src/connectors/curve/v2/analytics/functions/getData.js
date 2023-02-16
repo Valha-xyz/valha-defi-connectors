3a8b4e737bae5b6df4fe304682d3aa20790120b1
Thomas Cherret <b00705488@essec.edu>
2023-02-16 09:44:49 +0100

    automatic push

---
+++ src/connectors/curve/v2/analytics/functions/getData.js
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const axios = require('axios');
+const _ = require('lodash');
+
+function checkAPIType(id) {
+  if (id.includes('factory-crypto')) {
+    return 'factory-crypto';
+  } else if (id.includes('factory')) {
+    return 'factory';
+  } else if (id.includes('crypto')) {
+    return 'crypto';
+  } else {
+    return 'main';
+  }
+}
+
+async function getData(chain, poolAddress, id) {
+  try {
+    const type = checkAPIType(id);
+    const URL = `https://api.curve.fi/api/getPools/${chain}/${type}`;
+    const res = await axios.get(URL);
+    if (!res.data.success)
+      throw new Error(`Data from Angle indexer not ok for ${poolAddress}`);
+    const info = res.data.data.poolData;
+    const poolInfo = _.find(info, (elem) => {
+      return elem.id.toLowerCase() === id.toLowerCase();
+    });
+    console.log(poolInfo);
+
+    return {
+      data: poolInfo,
+      err: null,
+    };
+  } catch (err) {
+    console.log(err);
+    return { data: null, err };
+  }
+}
+
+module.exports = getData;
--- src/connectors/curve/v2/analytics/index.js
+++ src/connectors/curve/v2/analytics/index.js
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const _ = require('lodash');
-const external = require('./external/DefiLlama/index');
 const pools = require('../pools/pools');
-const checkSharePrice = require('./functions/sharePrice');
-const checkAngleV1APYTVL = require('./functions/apy');
+const getData = require('./functions/getData');
 
 async function analytics(chain, poolAddress) {
   const POOLS = await pools();
@@ -11,34 +9,44 @@ async function analytics(chain, poolAddress) {
   const poolInfo = _.find(POOLS, (elem) => {
     return elem.pool_address.toLowerCase() === poolAddress.toLowerCase();
   });
+  console.log(poolInfo);
 
-  const ActAPY = dataAPY ? dataAPY.activity_apy : 0;
-  const RewAPY = dataAPY ? dataAPY.rewards_apy : 0;
-  const totalAPY = ActAPY + RewAPY;
-  const TVL = dataAPY ? dataAPY.tvl : 0;
+  //Get data from the API
+  const info = await getData(chain, poolAddress, poolInfo.metadata.id);
+  console.log(info);
 
-  const result = {
-    status: null,
-    tvl: TVL,
-    liquidity: TVL,
-    outloans: null,
-    losses: null,
-    capacity: Number.MAX_SAFE_INTEGER,
-    apy: totalAPY,
-    activity_apy: ActAPY,
-    rewards_apy: RewAPY,
-    boosting_apy: null,
-    share_price: sharePrice.data,
-    minimum_deposit: null,
-    maximum_deposit: null,
-  };
+  // const sharePrice
 
-  console.log(result);
+  // //SHAREPRICE
+  // const ActAPY = dataAPY ? dataAPY.activity_apy : 0;
+  // const RewAPY = dataAPY ? dataAPY.rewards_apy : 0;
+  // const totalAPY = ActAPY + RewAPY;
+  // const TVL = dataAPY ? dataAPY.tvl : 0;
 
-  return result;
+  // const result = {
+  //   status: null,
+  //   tvl: TVL,
+  //   liquidity: TVL,
+  //   outloans: null,
+  //   losses: null,
+  //   capacity: Number.MAX_SAFE_INTEGER,
+  //   apy: totalAPY,
+  //   activity_apy: ActAPY,
+  //   rewards_apy: RewAPY,
+  //   boosting_apy: null,
+  //   share_price: sharePrice.data,
+  //   minimum_deposit: null,
+  //   maximum_deposit: null,
+  // };
+
+  // console.log(result);
+
+  // return result;
 }
 
-module.exports = {
-  main: analytics,
-  url: 'https://curve.fi/#/ethereum/pools',
-};
+// module.exports = {
+//   main: analytics,
+//   url: 'https://curve.fi/#/ethereum/pools',
+// };
+
+analytics('ethereum', '0x6c3f90f043a72fa612cbac8115ee7e52bde6e490');
