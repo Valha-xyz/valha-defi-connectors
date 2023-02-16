a7e0ed3a7b05b5075298cc7641936b7e4ed472c8
Thomas Cherret <b00705488@essec.edu>
2023-02-16 10:10:09 +0100

    automatic push

--- src/connectors/curve/v2/analytics/index.js
+++ src/connectors/curve/v2/analytics/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const _ = require('lodash');
-const { erc20Decimals } = require('src/utils/ERC20Decimals');
-const { getNodeProvider } = require('src/utils/getNodeProvider');
+const { erc20Decimals } = require('../../../../utils/ERC20Decimals');
+const { getNodeProvider } = require('../../../../utils/getNodeProvider');
 const pools = require('../pools/pools');
+const getActivityAPY = require('./functions/getActivityAPY');
 const getData = require('./functions/getData');
 
 async function analytics(chain, poolAddress) {
@@ -26,11 +27,15 @@ async function analytics(chain, poolAddress) {
   const sharePrice = info.virtualPrice / 10 ** decimals;
   const RewAPY =
     info.gaugeCrvApy && info.gaugeCrvApy.length > 0 ? info.gaugeCrvApy[0] : 0;
+  const BoostingAPY =
+    info.gaugeCrvApy && info.gaugeCrvApy.length > 1 ? info.gaugeCrvApy[1] : 0;
 
   //Get daily activity APY
-  const 
+  const infoActAPY = await getActivityAPY(chain, poolInfo.investing_address);
+  if (infoActAPY.err)
+    throw new Error(`Data from Curve V2 indexer not ok for ${poolAddress}`);
+  const ActAPY = infoActAPY.data;
 
-  
   const totalAPY = ActAPY + RewAPY;
 
   const result = {
@@ -43,15 +48,15 @@ async function analytics(chain, poolAddress) {
     apy: totalAPY,
     activity_apy: ActAPY,
     rewards_apy: RewAPY,
-    boosting_apy: null,
+    boosting_apy: BoostingAPY,
     share_price: sharePrice,
     minimum_deposit: null,
     maximum_deposit: null,
   };
 
-  // console.log(result);
+  console.log(result);
 
-  // return result;
+  return result;
 }
 
 module.exports = {
