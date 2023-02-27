06ee5020afdefc353835213b9e2f576823024a50
Thomas Cherret <b00705488@essec.edu>
2023-02-27 17:44:47 +0100

    automatic push

---
+++ src/connectors/pancakeswap/stableswap-v0/pools/pools.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+<<<<<<< HEAD
+const POOLS = require('./generatedPools.json')
+
+/// pools
+async function pools () {
+  return POOLS
+}
+
+module.exports = pools
+=======
+const POOLS = require('./generatedPools.json');
+
+/// pools
+async function pools() {
+  return POOLS;
+}
+
+module.exports = pools;
+>>>>>>> staging
