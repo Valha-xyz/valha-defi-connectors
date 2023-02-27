c60ae0a78b52935939ba21b2ecebdf27e7e26895
Thomas Cherret <b00705488@essec.edu>
2023-02-27 14:41:45 +0100

    automatic push

---
+++ src/connectors/gmx/v0/pools/pools.js
@@ -0,0 +1,21 @@
+const POOLS = [
+  {
+    name: 'Pool Name',
+    chain: 'ethereum',
+    underlying_tokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
+    pool_address: '0x0000000000000000000000000000000000000000',
+    investing_address: '0x0000000000000000000000000000000000000000',
+    staking_address: '0x0000000000000000000000000000000000000000',
+    boosting_address: null,
+    distributor_address: null,
+    rewards_tokens: ['0x0000000000000000000000000000000000000000'],
+    metadata: {}
+  }
+]
+
+/// pools
+async function pools () {
+  return POOLS
+}
+
+module.exports = pools
