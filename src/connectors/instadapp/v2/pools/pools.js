519944d55b4402e281609b5cd83468e45d422bb3
Thomas Cherret <b00705488@essec.edu>
2023-02-28 10:05:52 +0100

    automatic push

--- src/connectors/instadapp/v2/pools/pools.js
+++ src/connectors/instadapp/v2/pools/pools.js
@@ -1,21 +1,21 @@
 const POOLS = [
   {
-    name: 'Pool Name',
+    name: 'Pool Instadapp ETH v2',
     chain: 'ethereum',
-    underlying_tokens: ['0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'],
-    pool_address: '0x0000000000000000000000000000000000000000',
-    investing_address: '0x0000000000000000000000000000000000000000',
+    underlying_tokens: [''],
+    pool_address: '0xa0d3707c569ff8c87fa923d3823ec5d81c98be78',
+    investing_address: '0xa0d3707c569ff8c87fa923d3823ec5d81c98be78',
     staking_address: '0x0000000000000000000000000000000000000000',
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: ['0x0000000000000000000000000000000000000000'],
-    metadata: {}
-  }
-]
+    metadata: {},
+  },
+];
 
 /// pools
-async function pools () {
-  return POOLS
+async function pools() {
+  return POOLS;
 }
 
-module.exports = pools
+module.exports = pools;
