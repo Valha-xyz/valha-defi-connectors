39d8e119a15c63a283cb6a520e8acba8ca8c4a95
Thomas Cherret <b00705488@essec.edu>
2023-03-06 18:16:18 +0100

    automatic push

--- src/connectors/gmx/v0/pools/pools.js
+++ src/connectors/gmx/v0/pools/pools.js
@@ -5,10 +5,13 @@ const POOLS = [
     underlying_tokens: ['0xff970a61a04b1ca14834a43f5de4533ebddb5cc8'],
     pool_address: '0x1addd80e6039594ee970e5872d247bf0414c8903',
     investing_address: '0xb95db5b167d75e6d04227cfffa61069348d271f5',
-    staking_address: null,
+    staking_address: '0xa906f338cb21815cbc4bc87ace9e68c87ef8d8f1',
     boosting_address: null,
     distributor_address: null,
-    rewards_tokens: ['0xf42ae1d54fd613c9bb14810b0588faaa09a426ca'],
+    rewards_tokens: [
+      '0x82af49447d8a07e3bd95bd0d56f35241523fbab1',
+      '0xf42ae1d54fd613c9bb14810b0588faaa09a426ca',
+    ],
     metadata: {},
   },
 ];
