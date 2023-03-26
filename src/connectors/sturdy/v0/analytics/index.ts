e1d031c4b701e0986c25a6e40e1bd1133525fa71
Thomas Cherret <b00705488@essec.edu>
2023-03-26 21:30:06 +0200

    automatic push

--- src/connectors/sturdy/v0/analytics/index.ts
+++ src/connectors/sturdy/v0/analytics/index.ts
@@ -1,26 +1,26 @@
 import {
   type Analytics,
-  type AnalyticsExport
-} from '../../../../utils/types/connector-types'
-import { getVaults } from '../pools/updatePools'
+  type AnalyticsExport,
+} from '../../../../utils/types/connector-types';
+import { getVaults } from '../pools/updatePools';
 
 /* eslint-disable @typescript-eslint/no-var-requires */
-const _ = require('lodash')
+const _ = require('lodash');
 
-async function analytics (
+async function analytics(
   chain,
-  poolAddress
+  poolAddress,
 ): Promise<Analytics | Record<never, never>> {
   try {
-    const POOLS = await getVaults(chain)
-    const currentPool = POOLS.find((pool) => pool.address == poolAddress)
+    const POOLS = await getVaults(chain);
+    const currentPool = POOLS.find((pool) => pool.address == poolAddress);
 
-    if (!POOLS || POOLS.length === 0) return {}
+    if (!POOLS || POOLS.length === 0) return {};
 
-    const tvl = currentPool.tvl
-    const outloans = currentPool.totalBorrowUsd
-    const liquidity = tvl - outloans
-    const apy = currentPool.base
+    const tvl = currentPool.tvl;
+    const outloans = currentPool.totalBorrowUsd;
+    const liquidity = tvl - outloans;
+    const apy = currentPool.base * 100;
 
     const result = {
       status: currentPool.active,
@@ -35,18 +35,18 @@ async function analytics (
       boosting_apy: 0,
       share_price: 1,
       minimum_deposit: null,
-      maximum_deposit: null
-    }
+      maximum_deposit: null,
+    };
 
-    return result
+    return result;
   } catch (err) {
-    console.log(err)
-    return null
+    console.log(err);
+    return null;
   }
 }
 
 const analyticsExport: AnalyticsExport = {
   main: analytics,
-  url: 'https://app.aave.com/'
-}
-export default analyticsExport
+  url: 'https://app.aave.com/',
+};
+export default analyticsExport;
