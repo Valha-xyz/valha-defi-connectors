4f60bb9bd38ff4085cff2e25090cf0cda21b4042
Thomas Cherret <b00705488@essec.edu>
2023-04-13 14:57:14 +0200

    automatic push

--- src/connectors/morpho/aave-v0/pools/generatedPools.js
+++ src/connectors/morpho/aave-v0/pools/generatedPools.js
@@ -1,6 +1,6 @@
 const POOLS = [
   {
-    name: 'Pool maWBTC',
+    name: 'Pool maWBTC - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'],
     pool_address: '0xd508f85f1511aaec63434e26aeb6d10be0188dc7',
@@ -9,10 +9,12 @@ const POOLS = [
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0x9ff58f4ffb29fa2266ab25e75e2a8b3503311656',
+    },
   },
   {
-    name: 'Pool maWETH',
+    name: 'Pool maWETH - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'],
     pool_address: '0x490bbbc2485e99989ba39b34802fafa58e26aba4',
@@ -21,10 +23,12 @@ const POOLS = [
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0x030ba81f1c18d280636f32af80b9aad02cf0854e',
+    },
   },
   {
-    name: 'Pool maDAI',
+    name: 'Pool maDAI - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0x6b175474e89094c44da98b954eedeac495271d0f'],
     pool_address: '0x36f8d0d0573ae92326827c4a82fe4ce4c244cab6',
@@ -33,10 +37,12 @@ const POOLS = [
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0x028171bca77440897b824ca71d1c56cac55b68a3',
+    },
   },
   {
-    name: 'Pool maUSDC',
+    name: 'Pool maUSDC - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'],
     pool_address: '0xa5269a8e31b93ff27b887b56720a25f844db0529',
@@ -45,10 +51,12 @@ const POOLS = [
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0xbcca60bb61934080951369a648fb03df4f96263c',
+    },
   },
   {
-    name: 'Pool maUSDT',
+    name: 'Pool maUSDT - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0xdac17f958d2ee523a2206206994597c13d831ec7'],
     pool_address: '0xafe7131a57e44f832cb2de78ade38cad644aac2f',
@@ -57,10 +65,12 @@ const POOLS = [
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0x3ed3b47dd13ec9a98b44e6204a523e766b225811',
+    },
   },
   {
-    name: 'Pool maCRV',
+    name: 'Pool maCRV - Aave',
     chain: 'ethereum',
     underlying_tokens: ['0xd533a949740bb3306d119cc777fa900ba034cd52'],
     pool_address: '0x9dc7094530cb1bcf5442c3b9389ee386738a190c',
@@ -69,7 +79,9 @@ const POOLS = [
     boosting_address: null,
     distributor_address: '0xa7df60785e556d65292a2c9a077bb3a8fbf048bc',
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      protocolToken: '0x8dae6cb04688c62d939ed9b68d32bc62e49970b1',
+    },
   },
 ];
 
