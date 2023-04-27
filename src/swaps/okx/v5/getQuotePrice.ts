b6d49437581846246e804c1b7134fbe3bfc2a1be
Thomas Cherret <b00705488@essec.edu>
2023-04-27 22:14:22 +0200

    automatic push

--- src/swaps/okx/v5/getQuotePrice.ts
+++ src/swaps/okx/v5/getQuotePrice.ts
@@ -1,12 +1,12 @@
-import { BigNumber, type BigNumberish } from 'ethers'
-import axios from 'axios'
-import { getChainId } from '../../../utils/getChainId'
-import { type GetQuotePriceFunction } from '../../../utils/types/quotePrice'
+import { BigNumber, type BigNumberish } from 'ethers';
+import axios from 'axios';
+import { getChainId } from '../../../utils/getChainId';
+import { type GetQuotePriceFunction } from '../../../utils/types/quotePrice';
 // DOC is located here : https://www.okx.com/id/web3-docs/dex/dex_api
 
-const okxInchAPI = axios.create({
-  baseURL: 'https://www.okx.com/api/v5/dex/aggregator'
-})
+const okxAPI = axios.create({
+  baseURL: 'https://www.okx.com/api/v5/dex/aggregator',
+});
 
 export const getQuotePrice: GetQuotePriceFunction = async (
   tokenIn: string,
@@ -14,15 +14,15 @@ export const getQuotePrice: GetQuotePriceFunction = async (
   tokenOut: string,
   chain: string
 ): Promise<BigNumber> => {
-  const chainId = getChainId(chain)
-  const quote = await okxInchAPI.get(`quote`, {
+  const chainId = getChainId(chain);
+  const quote = await okxAPI.get('quote', {
     params: {
       chainId,
       amount: amount.toString(),
       fromTokenAddress: tokenIn,
       toTokenAddress: tokenOut,
-    }
-  })
+    },
+  });
 
-  return BigNumber.from(quote.data.data[0].toTokenAmount)
-}
+  return BigNumber.from(quote.data.data[0].toTokenAmount);
+};
--- src/swaps/okx/v5/getSwapCalldata.ts
+++ src/swaps/okx/v5/getSwapCalldata.ts
@@ -1,15 +1,18 @@
-import { BigNumber, type BigNumberish } from 'ethers'
-import axios from 'axios'
-import { getChainId } from '../../../utils/getChainId'
-import { SwapOptions, type GetSwapCalldataFunction } from '../../../utils/types/liquidityProviders'
-import { ONE_INCH_SLIPPAGE } from 'src/swaps/oneinch/v5/getSwapCalldata'
+import { BigNumber, type BigNumberish } from 'ethers';
+import axios from 'axios';
+import { getChainId } from '../../../utils/getChainId';
+import {
+  type SwapOptions,
+  type GetSwapCalldataFunction,
+} from '../../../utils/types/liquidityProviders';
+import { ONE_INCH_SLIPPAGE } from 'src/swaps/oneinch/v5/getSwapCalldata';
 // DOC is located here : https://www.okx.com/id/web3-docs/dex/dex_api
 
-const  okxInchAPI = axios.create({
-  baseURL: 'https://www.okx.com/api/v5/dex/aggregator'
-})
+const okxAPI = axios.create({
+  baseURL: 'https://www.okx.com/api/v5/dex/aggregator',
+});
 
-const OKX_SLIPPAGE_FACTOR = 100
+const OKX_SLIPPAGE_FACTOR = 100;
 
 export const getSwapCalldata: GetSwapCalldataFunction = async (
   chain: string,
@@ -19,38 +22,39 @@ export const getSwapCalldata: GetSwapCalldataFunction = async (
   swapperAddress: string,
   options: SwapOptions
 ) => {
-  const chainId = getChainId(chain)
+  const chainId = getChainId(chain);
   if (!chainId) {
     throw new Error(
       `Swap not supported for this protocol yet, protocol was: ${chain}`
-    )
+    );
   }
-  const swapData = await okxInchAPI
-    .get(`swap`, {
+  const swapData = await okxAPI
+    .get('swap', {
       params: {
         chainId,
         amount: amount.toString(),
         fromTokenAddress: tokenIn,
         toTokenAddress: tokenOut,
         userWalletAddress: swapperAddress,
-        slippage: (options?.slippage ?? ONE_INCH_SLIPPAGE)/OKX_SLIPPAGE_FACTOR,
-      }
+        slippage:
+          (options?.slippage ?? ONE_INCH_SLIPPAGE) / OKX_SLIPPAGE_FACTOR,
+      },
     })
     .catch((error) => {
-      console.log(error)
-      return error
-    })
+      console.log(error);
+      return error;
+    });
 
   if (!swapData?.data) {
     throw new Error(
       'There was an error while trying to generate the swap tx data for 1inch.'
-    )
+    );
   }
-  if(swapData.data.msg && swapData.data.data.length == 0){
-    throw swapData.data.msg
+  if (swapData.data.msg && swapData.data.data.length == 0) {
+    throw swapData.data.msg;
   }
-  console.log(swapData.data.data[0].tx.to)
+  console.log(swapData.data.data[0].tx.to);
   return {
-    data: swapData.data.data[0].tx.data
-  }
-}
+    data: swapData.data.data[0].tx.data,
+  };
+};
