ea67b6f3261938713536770bca125945bad40a0f
Kayanski <kowalski.kowalskin@gmail.com>
2023-04-28 11:06:39 +0200

    Corrected Uniswap pool generation

--- src/connectors/uniswap/v3/pools/updatePools.ts
+++ src/connectors/uniswap/v3/pools/updatePools.ts
@@ -13,31 +13,39 @@ const SUBGRAPH_URLS = {
   fantom: ''
   */
 }
-const poolsQuery = gql`
-  query Query{
-    pools(first: 1){
-      id
-      
-      token0{
-        id
-        name
-      }
-      token1{
-        id
-        name
-      }
-      feeTier
-    }
+function poolsQuery(last_id?: string){
+
+  let lastIdCondition = `where: { id_gt: "${last_id}" }`
+  if(last_id == undefined){
+    lastIdCondition = "";
   }
-`
+
+  return gql`
+      query Query{
+        pools(first: 1000 ${lastIdCondition}){
+          id
+          
+          token0{
+            id
+            name
+          }
+          token1{
+            id
+            name
+          }
+          feeTier
+        }
+      }
+    `
+}
 
 // This is the general interaction address (true for all pools). It handles all liquidity providing/collecting/retrieving
 const NFTPositionManager = "0xC36442b4a4522E871399CD717aBDD847Ab11FE88";
  
-async function getPools (chain) {
+async function getPools (chain: string, last_id?: string) {
   try {
     const SUBGRAPH_URL = SUBGRAPH_URLS[chain]
-    const pools = await request(SUBGRAPH_URL, poolsQuery)
+    const pools = await request(SUBGRAPH_URL, poolsQuery(last_id))
 
     // We create the pools object
     const formattedPools = pools.pools.map((pool) : Pool => {
@@ -64,9 +72,23 @@ async function getPools (chain) {
   }
 }
 
+async function getAllPools(chain: string){
+  let lastId: string;
+  let allPools = []
+  let newPools = []
+  do{
+    newPools = await getPools(chain, lastId);
+    allPools = allPools.concat(newPools);
+    lastId = allPools[allPools.length - 1].pool_address;
+    console.log(lastId)
+    console.log(allPools[allPools.length - 1])
+  }while(newPools.length != 0)
+
+  return allPools;
+}
 
 async function generatePools(){
-  const allPools = await pMap(Object.keys(SUBGRAPH_URLS), async (chain)=>getPools(chain));
+  const allPools = await pMap(Object.keys(SUBGRAPH_URLS), async (chain)=>getAllPools(chain));
   return allPools.flat()
 }
 
