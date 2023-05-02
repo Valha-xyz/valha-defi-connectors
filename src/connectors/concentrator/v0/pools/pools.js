e267850fc29be8d6c58373edd602c8f2ebebdbd3
Thomas Cherret <b00705488@essec.edu>
2023-05-02 12:03:59 +0200

    automatic push

--- src/connectors/concentrator/v0/pools/pools.js
+++ src/connectors/concentrator/v0/pools/pools.js
@@ -2,14 +2,16 @@ const POOLS = [
   {
     name: 'Aladin Stake DAO CRV',
     chain: 'ethereum',
-    underlying_tokens: ['0xd533a949740bb3306d119cc777fa900ba034cd52'],
+    underlying_tokens: ['0xd1b5651e55d4ceed36251c61c50c889b36f6abb5'],
     pool_address: '0x43e54c2e7b3e294de3a155785f52ab49d87b9922',
     investing_address: '0x43e54c2e7b3e294de3a155785f52ab49d87b9922',
     staking_address: null,
     boosting_address: null,
     distributor_address: null,
     rewards_tokens: [],
-    metadata: {},
+    metadata: {
+      initial_tokens: ['0xd533a949740bb3306d119cc777fa900ba034cd52'],
+    },
   },
 ];
 
