5169b0a3438000c305390ec52e4ececd4d12d1c7
Thomas Cherret <b00705488@essec.edu>
2023-06-05 14:02:38 +0200

    automatic push

--- src/connectors/convex/arbitrum-v0/analytics/functions/getConvexData.js
+++ src/connectors/convex/arbitrum-v0/analytics/functions/getConvexData.js
@@ -7,13 +7,13 @@ async function getConvexData(chain, poolAddress, convexId) {
   try {
     // Get right underlying ID from Curve for the API
     const id = 'arbitrum-' + poolAddress.toLowerCase() + '-' + convexId;
-    //Get URL Data
-    const URL = 'https://www.convexfinance.com/api/sidechains-apys';
+    // Get URL Data
+    const URL = 'https://www.convexfinance.com/api/curve-arbitrum-apys';
     const { data } = await axios.get(URL);
     if (!data || !data.apys) {
       throw new Error(`Data from Convex V0 indexer not ok for ${poolAddress}`);
     }
-    let info = data.apys;
+    const info = data.apys;
     const result = info[id];
     return { data: result, err: null };
   } catch (err) {
