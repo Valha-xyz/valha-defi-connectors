5cc1ce00d3868d3e982df5ccc14813037ce1b071
octavionotpunk <octave.mesnard@gmail.com>
2023-06-20 18:29:33 +0200

    compound data onchain

--- src/connectors/compound/v2/analytics/index.js
+++ src/connectors/compound/v2/analytics/index.js
@@ -6,11 +6,45 @@ const { getNodeProvider } = require('../../../../utils/getNodeProvider');
 const {
   getGeckoTokenPrice,
 } = require('../../../../utils/prices/getGeckoTokenPrice');
-const checkCompoundv2Data = require('./functions/getData');
+const checkCompoundV2Data = require('./functions/getData');
+const checkCompoundV2Rewards = require('./functions/getReward');
 const checkCompoundV2TVL = require('./functions/tvl');
 const checkCompoundV2Liquidity = require('./functions/liquidity');
 const checkCompoundV2Outloans = require('./functions/outloans');
 const checkCompoundV2Share = require('./functions/sharePrice');
+
+const BLOCKS_PER_DAY = 86400 / 12;
+
+const COMP = {
+  decimals: 18,
+  symbol: 'COMP',
+  address: ['0xc00e94Cb662C3520282E6f5717214004A7f26888'],
+};
+
+// function calculateApy(rate, price=1, tvl=1) {
+//   // supply rate per block * number of blocks per year
+//   const BLOCK_TIME = 3;
+//   const YEARLY_BLOCKS = (365 * 24 * 60 * 60) / BLOCK_TIME;
+//   const apy = ((rate * YEARLY_BLOCKS * price) / tvl) * 100;
+//   return apy;
+// }
+
+const calculateApy = (ratePerTimestamps) => {
+  const blocksPerDay = BLOCKS_PER_DAY;
+  const daysPerYear = 365;
+  return (
+    (Math.pow(ratePerTimestamps * blocksPerDay + 1, daysPerYear) - 1) * 100
+  );
+};
+
+const calcRewardApy = (rewards, TVL, COMP_PRICE) => {
+  return (
+    ((rewards *BLOCKS_PER_DAY *365*COMP_PRICE) / TVL) *
+    100
+  );
+};
+
+
 
 async function analytics(chain, poolAddress) {
   // Find information about underlying token.
@@ -28,7 +62,7 @@ async function analytics(chain, poolAddress) {
     throw new Error('Error: Compound underlying decimals null.');
 
   // Find information about token price
-  const { data, err } = await getGeckoTokenPrice(chain, underlyingToken);
+  const { data, err } =  await getGeckoTokenPrice(chain, underlyingToken);
   if (err) throw new Error(err.message);
   const tokenPrice = data;
 
@@ -55,13 +89,20 @@ async function analytics(chain, poolAddress) {
   const Liquidity = LiquidityNative.data * tokenPrice;
 
   // Find information on Compound API.
-  const info = await checkCompoundv2Data(chain, poolAddress);
-  const ActAPY = info['supply_rate'] ? info['supply_rate'].value * 100 : 0;
-  const RewAPY = info['comp_supply_apy']
-    ? parseFloat(info['comp_supply_apy'].value)
-    : 0;
+
+  const info = await checkCompoundV2Data(chain, poolAddress);
+  const ActAPY = info.data.apyBase ? calculateApy(info.data.apyBase) : 0;
+
+   // Find information about VXS Price
+  const { price2, err2 } = await getGeckoTokenPrice(chain, COMP.address[0]);
+  if (err2) throw new Error(err.message);
+  const COMP_PRICE = price2 ? price2 : 20; // Hardwrite asset price
+
+  const reward = await checkCompoundV2Rewards(chain, poolAddress);
+  const RewAPY = reward.data.apyRewards ? calcRewardApy(reward.data.apyRewards, TVL, COMP_PRICE) : 0;
   const totalAPY = ActAPY + RewAPY;
 
+  
   const result = {
     status: null,
     tvl: TVL,
@@ -77,6 +118,8 @@ async function analytics(chain, poolAddress) {
     minimum_deposit: null,
     maximum_deposit: null,
   };
+
+  console.log(result);
 
   return result;
 }
--- tests/connectors/config/testPools.ts
+++ tests/connectors/config/testPools.ts
@@ -1,6 +1,6 @@
 
         import {Pool} from "../../../src/utils/types/connector-types"
 
-        export const POOLS: Pool[] = [{"name":"Pool USDT","chain":"arbitrum","underlying_tokens":["0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9"],"pool_address":"0xd69D402D1bDB9A2b8c3d88D98b9CEaf9e4Cd72d9","investing_address":"0xf4b1486dd74d07706052a33d31d7c0aafd0659e1","staking_address":null,"boosting_address":null,"distributor_address":"0xebC85d44cefb1293707b11f707bd3CEc34B4D5fA","rewards_tokens":["0x3082cc23568ea640225c2467653db90e9250aaa0"],"metadata":{}}]
+        export const POOLS: Pool[] = [{"name":"Pool V2 cUSDC","chain":"ethereum","underlying_tokens":["0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"],"pool_address":"0x39aa39c021dfbae8fac545936693ac917d5e7563","investing_address":"0x39aa39c021dfbae8fac545936693ac917d5e7563","staking_address":"","boosting_address":null,"distributor_address":"0x3d9819210a31b4961b30ef54be2aed79b9c9cd3b","rewards_tokens":["0xc00e94cb662c3520282e6f5717214004a7f26888"],"metadata":{}}]
 
       
