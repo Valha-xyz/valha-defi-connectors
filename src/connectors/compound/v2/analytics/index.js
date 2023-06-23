3165181152f488d5b95f23bb5be103491281b9d3
Thomas Cherret <b00705488@essec.edu>
2023-06-23 16:41:51 +0200

    automatic push

--- src/connectors/compound/v2/analytics/index.js
+++ src/connectors/compound/v2/analytics/index.js
@@ -6,11 +6,40 @@ const { getNodeProvider } = require('../../../../utils/getNodeProvider');
 const {
   getGeckoTokenPrice,
 } = require('../../../../utils/prices/getGeckoTokenPrice');
-const checkCompoundv2Data = require('./functions/getData');
+const checkCompoundV2Data = require('./functions/getData');
+const checkCompoundV2Rewards = require('./functions/getReward');
 const checkCompoundV2TVL = require('./functions/tvl');
 const checkCompoundV2Liquidity = require('./functions/liquidity');
 const checkCompoundV2Outloans = require('./functions/outloans');
 const checkCompoundV2Share = require('./functions/sharePrice');
+
+const BLOCKS_PER_DAY = 86400 / 12;
+
+const COMP = {
+  decimals: 18,
+  symbol: 'COMP',
+  address: ['0xc00e94Cb662C3520282E6f5717214004A7f26888'],
+};
+
+// function calculateApy(rate, price=1, tvl=1) {
+//   // supply rate per block * number of blocks per year
+//   const BLOCK_TIME = 3;
+//   const YEARLY_BLOCKS = (365 * 24 * 60 * 60) / BLOCK_TIME;
+//   const apy = ((rate * YEARLY_BLOCKS * price) / tvl) * 100;
+//   return apy;
+// }
+
+const calculateApy = (ratePerTimestamps) => {
+  const blocksPerDay = BLOCKS_PER_DAY;
+  const daysPerYear = 365;
+  return (
+    (Math.pow(ratePerTimestamps * blocksPerDay + 1, daysPerYear) - 1) * 100
+  );
+};
+
+const calcRewardApy = (rewards, TVL, COMP_PRICE) => {
+  return ((rewards * BLOCKS_PER_DAY * 365 * COMP_PRICE) / TVL) * 100;
+};
 
 async function analytics(chain, poolAddress) {
   // Find information about underlying token.
@@ -20,12 +49,14 @@ async function analytics(chain, poolAddress) {
     return elem.pool_address.toLowerCase() === poolAddress.toLowerCase();
   });
   const underlyingToken = poolInfo.underlying_tokens[0];
-  if (!underlyingToken)
+  if (!underlyingToken) {
     throw new Error('Error: no underlying found for Compound');
+  }
   const provider = getNodeProvider(chain);
   const underlyingDecimals = await erc20Decimals(provider, underlyingToken);
-  if (underlyingDecimals === 0)
+  if (underlyingDecimals === 0) {
     throw new Error('Error: Compound underlying decimals null.');
+  }
 
   // Find information about token price
   const { data, err } = await getGeckoTokenPrice(chain, underlyingToken);
@@ -55,10 +86,18 @@ async function analytics(chain, poolAddress) {
   const Liquidity = LiquidityNative.data * tokenPrice;
 
   // Find information on Compound API.
-  const info = await checkCompoundv2Data(chain, poolAddress);
-  const ActAPY = info['supply_rate'] ? info['supply_rate'].value * 100 : 0;
-  const RewAPY = info['comp_supply_apy']
-    ? parseFloat(info['comp_supply_apy'].value)
+
+  const info = await checkCompoundV2Data(chain, poolAddress);
+  const ActAPY = info.data.apyBase ? calculateApy(info.data.apyBase) : 0;
+
+  // Find information about VXS Price
+  const { price2, err2 } = await getGeckoTokenPrice(chain, COMP.address[0]);
+  if (err2) throw new Error(err.message);
+  const COMP_PRICE = price2 || 20; // Hardwrite asset price
+
+  const reward = await checkCompoundV2Rewards(chain, poolAddress);
+  const RewAPY = reward.data.apyRewards
+    ? calcRewardApy(reward.data.apyRewards, TVL, COMP_PRICE)
     : 0;
   const totalAPY = ActAPY + RewAPY;
 
@@ -77,6 +116,8 @@ async function analytics(chain, poolAddress) {
     minimum_deposit: null,
     maximum_deposit: null,
   };
+
+  console.log(result);
 
   return result;
 }
