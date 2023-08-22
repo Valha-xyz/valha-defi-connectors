84587fe78f017b139e6f31ec951ecd1279a12c2d
octavionotpunk <octave.mesnard@gmail.com>
2023-08-22 13:57:23 +0200

    change maker-dsr

--- src/connectors/maker-dsr/v0/analytics/index.js
+++ src/connectors/maker-dsr/v0/analytics/index.js
@@ -1,76 +1,102 @@
 
 
-const ERC4626ABI = require('../abi/Pool');
-const PotABI = require('../abi/Pot');
-const { getNodeProvider } = require('src/utils/getNodeProvider');
+const { PoolABI } = require('../abi/Pool');
+const { PotABI } = require('../abi/Pot');
+const { getNodeProvider } = require('../../../../utils/getNodeProvider');
 const ethers = require('ethers');
 const POT_ADDRESS = '0x197e90f9fad81970ba7976f33cbd77088e5d7cf7'
 const RAY_PRECISION = 27;
 const RAY = 10 ** RAY_PRECISION;
 
-
-async function getStatus(chain, pool_address) {
-  return true;
-}
-
-async function getAPY(POT) {
+async function getAPY(chain) {
+  try {
+  const provider = getNodeProvider(chain);
+  if (!provider) throw new Error('No provider was found.');
+  const POT = new ethers.Contract(POT_ADDRESS, PotABI, provider);
+  
   const dsr = await POT.dsr();
   const activity_apy = (dsr/RAY) ** (60 * 60 * 24 * 365) - 1;
-  const rewards_apy = 0;
-  return { activity_apy: activity_apy, rewards_apy: rewards_apy };
+
+  return {data: activity_apy, err: null };
+} catch (err) {
+  console.log(err);
+  return { data: null, err };
+}
 }
 
-async function getTotalAssets(POOL) {
-  const decimals = await POOL.decimals();
-  const TotalAssets = await POOL.totalAssets();
-  return TotalAssets / 10 ** decimals;
-}
+// async function getTotalAssets(POOL) {
+//   const decimals = await POOL.decimals();
+//   const TotalAssets = await POOL.totalAssets();
+//   return TotalAssets / 10 ** decimals;
+// }
 
-async function getSharePrice(POOL) {
+async function getSharePrice(poolAddress,chain) {
+  try {
+  
+  const provider = getNodeProvider(chain);
+  if (!provider) throw new Error('No provider was found.');
+  const POOL = new ethers.Contract(poolAddress, PoolABI, provider);
+
   const TotalAssets = await POOL.totalAssets();
   const TotalSupply = await POOL.totalSupply();
-  return TotalAssets / TotalSupply;
+  return { data: TotalAssets / TotalSupply, err: null };
+} catch (err) {
+  console.log(err);
+  return { data: null, err };
+}
 }
 
-async function getTVL(POT) {
+async function getTVL(chain) {
+  try {
+
+  const provider = getNodeProvider(chain);
+  if (!provider) throw new Error('No provider was found.');
+  const POT = new ethers.Contract(POT_ADDRESS, PotABI, provider);
+
   const Pie = await POT.Pie();
   const chi = await POT.chi();
   const tvlUsd = Pie * chi / (1e18 * RAY);
-  return tvlUsd;
+  return { data: tvlUsd, err: null };
+} catch (err) {
+  console.log(err);
+  return { data: null, err };
+}
 }
 
 
 async function analytics(chain, poolAddress) {
-  const provider = getNodeProvider(chain);
-  if (!provider) throw new Error('No provider was found.');
-  const POOL = new ethers.Contract(poolAddress, ERC4626ABI, provider);
-  console.log(POOL);
-  if (!POOL) throw new Error('Pool was not found');
-  const POT = new ethers.Contract(POT_ADDRESS, PotABI, provider);
-  if (!POT) throw new Error('Pot was not found');
-  const TVL = await getTVL(POT);
-  const status = await getStatus(POOL);
-  const { activity_apy, rewards_apy } = await getAPY(POT);
-  const sharePrice = await getSharePrice(chain, poolAddress);
+  try {
+  
+  const TVL = await getTVL(chain);
+  if (TVL.err) throw new Error(TVL.err);
+  const activity_apy = await getAPY(chain);
+  if (activity_apy.err) throw new Error(activity_apy.err);
+  const sharePrice = await getSharePrice(poolAddress,chain);
+  if (sharePrice.err) throw new Error(sharePrice.err);
 
   const result = {
-    status: status,
-    tvl: TVL,
-    liquidity: TVL,
+    status: true,
+    tvl: TVL.data,
+    liquidity: TVL.data,
     outloans: null,
     losses: null,
-    capacity: 1e18,
-    apy: activity_apy + rewards_apy,
-    activity_apy: activity_apy,
-    rewards_apy: rewards_apy,
+    capacity: Number.MAX_SAFE_INTEGER,
+    apy: activity_apy.data,
+    activity_apy: activity_apy.data,
+    rewards_apy: 0,
     boosting_apy: 0,
-    share_price: sharePrice,
+    share_price: sharePrice.data,
     minimum_deposit: null,
     maximum_deposit: null,
   };
-
-  return result;
+  console.log(result)
+  return result
+} catch (err) {
+  console.log(err);
+  return {};
 }
+}
+
 
 module.exports = {
   main: analytics,
--- tests/connectors/config/testPools.ts
+++ tests/connectors/config/testPools.ts
@@ -1,6 +1,6 @@
 
         import {Pool} from "../../../src/utils/types/connector-types"
 
-        export const POOLS: Pool[] = [{"name":"Pool DAI","chain":"ethereum","underlying_tokens":["0x6b175474e89094c44da98b954eedeac495271d0f"],"pool_address":"0x4dedf26112b3ec8ec46e7e31ea5e123490b05b8b","investing_address":"0xc13e21b648a5ee794902342038ff3adab66be987","staking_address":null,"boosting_address":null,"distributor_address":null,"rewards_tokens":[],"metadata":{}}]
+        export const POOLS: Pool[] = [{"name":"sDai","chain":"ethereum","underlying_tokens":["0x6b175474e89094c44da98b954eedeac495271d0f"],"pool_address":"0x83f20f44975d03b1b09e64809b757c47f942beea","investing_address":"0x83f20f44975d03b1b09e64809b757c47f942beea","staking_address":null,"boosting_address":null,"distributor_address":null,"rewards_tokens":[],"metadata":{}}]
 
       
