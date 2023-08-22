49dc67b5318863a2e6b4fac1092a6d972662e8e1
octavionotpunk <octave.mesnard@gmail.com>
2023-08-22 10:54:30 +0200

    add frax ether

--- tests/connectors/config/testPools.ts
+++ tests/connectors/config/testPools.ts
@@ -1,6 +1,6 @@
 
         import {Pool} from "../../../src/utils/types/connector-types"
 
-        export const POOLS: Pool[] = [{"name":"sETH2","chain":"ethereum","underlying_tokens":["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],"pool_address":"0xfe2e637202056d30016725477c5da089ab0a043a","investing_address":"0xC874b064f465bdD6411D45734b56fac750Cda29A","staking_address":null,"boosting_address":null,"distributor_address":null,"rewards_tokens":[],"metadata":{}}]
+        export const POOLS: Pool[] = [{"name":"frxETH","chain":"ethereum","underlying_tokens":["0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"],"pool_address":"0x5e8422345238f34275888049021821e8e08caa1f","investing_address":"0xbafa44efe7901e04e39dad13167d089c559c1138","staking_address":"0xac3e018457b222d93114458476f3e3416abbe38f","boosting_address":null,"distributor_address":null,"rewards_tokens":[],"metadata":{}}]
 
       
