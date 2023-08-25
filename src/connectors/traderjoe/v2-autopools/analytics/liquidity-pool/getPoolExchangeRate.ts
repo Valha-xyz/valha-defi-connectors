4c128f8a34ff9b38f24dfe9c431f40efa1d4c6b1
Thomas Cherret <b00705488@essec.edu>
2023-08-25 15:21:28 +0200

    automatic push

--- src/connectors/traderjoe/v2-autopools/analytics/liquidity-pool/getPoolExchangeRate.ts
+++ src/connectors/traderjoe/v2-autopools/analytics/liquidity-pool/getPoolExchangeRate.ts
@@ -1,31 +1,31 @@
-import { BigNumber, BigNumberish, Contract } from 'ethers'
-import { getNodeProvider } from '../../../../../utils/getNodeProvider'
+import { BigNumber, BigNumberish, Contract } from 'ethers';
+import { getNodeProvider } from '../../../../../utils/getNodeProvider';
+import { type Pool } from '../../../../../utils/types/connector-types';
+import { type GetExchangeRateFunction } from '../../../../../utils/types/liquidityProviders';
 const { POOLABI } = require('../../abi/POOL');
-import { type Pool } from '../../../../../utils/types/connector-types'
-import { type GetExchangeRateFunction } from '../../../../../utils/types/liquidityProviders'
 
 // We only want to know the exchange rate between two assets.
 // This should return the amount of token2 equivalent to amount1 token1
 // In the best case scenario, this function does not need the amount1 parameter and only relies on pool reserves
 // This function does not assume token1 != token2
-export const getExchangeRate: GetExchangeRateFunction = async (
-  amount1: Number,
+export async function getExchangeRate(
+  amount1: number,
   token1: string,
   token2: string,
   pool: Pool
-): Promise<Number> => {
-  const provider = getNodeProvider(pool.chain)
+): Promise<number> {
+  const provider = getNodeProvider(pool.chain);
   const liquidityProvidingContract = new Contract(
     pool.investing_address,
     POOLABI,
     provider
-  )
-  if (token1 == token2) {
-    return amount1
+  );
+  if (token1 === token2) {
+    return amount1;
   }
 
   // First we get the reserves
-  const officialPrice = await liquidityProvidingContract.getPrice()
-  const price =  Number(amount1) * officialPrice
+  const officialPrice = await liquidityProvidingContract.getPrice();
+  const price = Number(amount1) * officialPrice;
   return price;
 }
