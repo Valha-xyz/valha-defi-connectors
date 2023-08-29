a402fb899c055f5d7ae8553f0612678369419695
octavionotpunk <octave.mesnard@gmail.com>
2023-08-29 20:24:33 +0200

    add notional analytics (balancer not ready)

---
+++ src/connectors/balancer/v2/analytics/functions/external/graphQuery.js
@@ -0,0 +1,69 @@
+const { request, gql } = require('graphql-request');
+const axios = require ('axios')
+
+export async function queryGraphData(SUBGRAPH_URL, poolAddress,block){
+
+  const poolsQuery = gql`
+            { pairs(where: { id: "<IDHOLDER>" }, orderBy: trackedReserveETH, orderDirection: desc block: {number: <PLACEHOLDER>}) {
+                id
+                reserve0
+                reserve1
+                reserveUSD
+                volumeUSD
+                token0 {
+                  symbol
+                  id
+                }
+                token1 {
+                  symbol
+                  id
+                }
+              }
+            }`
+  
+  const poolsQueryModified = poolsQuery.replace('<IDHOLDER>',poolAddress)
+  const res  = await request(SUBGRAPH_URL, poolsQueryModified.replace('<PLACEHOLDER>', block))
+  return res.pairs
+    }
+
+
+export function histo(pool, dataPrior7d, version){
+    // uni v2 forks set feeTier to constant
+    if (version === 'v2') {
+      pool['feeTier'] = 3000;
+    } else if (version === 'stellaswap') {
+      pool['feeTier'] = 2000;
+    } else if (version === 'baseswap') {
+      pool['feeTier'] = 1700;
+    } else if (version === 'zyberswap') {
+      pool['feeTier'] = 1500;
+    } else if (version === 'arbidex') {
+      pool['feeTier'] = 500;
+    }
+    pool['volumeUSDPrior7d'] = dataPrior7d[0].volumeUSD;
+    // calc 24h volume
+    pool['volumeUSD7d'] = Number(pool[0].volumeUSD) - Number(pool.volumeUSDPrior7d);
+    // calc fees
+    pool['feeUSD7d'] = (Number(pool.volumeUSD7d) * Number(pool.feeTier)) / 1e6;
+    // annualise
+    pool['feeUSDyear7d'] = pool.feeUSD7d * 52
+    // calc apy
+    pool['apy7d'] = (pool.feeUSDyear7d / pool[0].reserveUSD) * 100;
+
+    return pool;
+  }
+
+export async function getBlocksByTime(timestamp, chainString){
+    try {
+      const chain = chainString === 'avalanche' ? 'avax' : chainString;
+      const response = await axios.get(`https://coins.llama.fi/block/${chain}/${timestamp}`)
+      return response.data.height
+    }
+    catch (err) {
+      console.log(err);
+      return { data: null, err };
+    }
+  }
+
+ 
+  
---
+++ src/connectors/balancer/v2/analytics/functions/sharePrice.js
@@ -0,0 +1,60 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const _ = require('lodash');
+const {queryGraphData, getBlocksByTime} = require ('./external/graphQuery');
+const ERC20ABI = require('../../../../../utils/abi/ERC20.json');
+const pools = require('../../pools/pools');
+const { getNodeProvider } = require('../../../../../utils/getNodeProvider');
+const { ethers } = require('ethers');
+
+
+const SUBGRAPH_URLS = {
+  ethereum: "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v2-dev",
+}
+async function checkUniV2SharePrice(chain, poolAddress) {
+  try {
+    const POOLS = await pools();
+    if (!POOLS || POOLS.length === 0) return {};
+
+    const poolInfo = _.find(POOLS, (elem) => {
+      return elem.pool_address.toLowerCase() === poolAddress.toLowerCase();
+    });
+
+    const underlying_tokens = poolInfo.underlying_tokens;
+
+    const currentTimestamp = Math.floor(Date.now() / 1000) - 100;
+
+    const SUBGRAPH_URL = SUBGRAPH_URLS[chain];
+    const currentBlock = await getBlocksByTime(currentTimestamp,chain);
+    const query = await queryGraphData(SUBGRAPH_URL,poolAddress,currentBlock);
+    
+
+    // We get the current pool inside this big array
+    const TVL = Number(query[0].reserveUSD);
+    const TVL0 = 2 * Number(query[0].reserve0);
+    const TVL1 = 2 * Number(query[0].reserve1);
+
+
+    const provider = getNodeProvider(chain);
+    if (!provider) throw new Error('No provider was found.');
+
+    const poolToken = new ethers.Contract(poolAddress, ERC20ABI, provider);
+    const supplyBN = await poolToken.totalSupply();
+    const decimalsBN = await poolToken.decimals();
+    const supply = supplyBN / 10 ** decimalsBN;
+
+
+    
+
+    const share_price = { sharePriceUSD: TVL/supply, sharePriceToken0: TVL0/supply, sharePriceToken1 :TVL1/supply }
+
+
+
+
+    return { data: share_price, err: null };
+  } catch (err) {
+    console.log(err);
+    return { data: null, err };
+  }
+}
+
+module.exports = checkUniV2SharePrice;
