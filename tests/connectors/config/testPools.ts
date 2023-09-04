443fa2a1a753901c47705fa059d95fccbf38fa21
octavionotpunk <octave.mesnard@gmail.com>
2023-09-04 19:37:55 +0200

    add balancer v2 sdk interactions

--- tests/connectors/config/testPools.ts
+++ tests/connectors/config/testPools.ts
@@ -1,6 +1,6 @@
 
         import {Pool} from "../../../src/utils/types/connector-types"
 
-        export const POOLS: Pool[] = [{"name":"wBETH","chain":"bsc","underlying_tokens":["0x2170ed0880ac9a755fd29b2688956bd959f933f8"],"pool_address":"0xa2E3356610840701BDf5611a53974510Ae27E2e1","investing_address":"0xa2E3356610840701BDf5611a53974510Ae27E2e1","staking_address":null,"boosting_address":null,"distributor_address":null,"rewards_tokens":[],"metadata":{}}]
+        export const POOLS: Pool[] = [{"name":"Balancer rETH Stable Pool","chain":"ethereum","underlying_tokens":["0xae78736cd615f374d3085123a210448e74fc6393","0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"],"investing_address":"0xba12222222228d8ba445958a75a0704d566bf2c8","pool_address":"0x1e19cf2d73a72ef1332c882f20534b6519be0276","staking_address":"0x79ef6103a513951a3b25743db509e267685726b7","boosting_address":null,"distributor_address":"0x79ef6103a513951a3b25743db509e267685726b7","rewards_tokens":["0xba100000625a3754423978a60c9317c58a424e3D"],"metadata":{}}]
 
       
