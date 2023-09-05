844d9cba92389b7629ed36bea3db870d5953b8c2
octavionotpunk <octave.mesnard@gmail.com>
2023-09-05 10:27:23 +0200

    change polygon stader labs

--- src/connectors/stader/polygon-v0/pools/pools.js
+++ src/connectors/stader/polygon-v0/pools/pools.js
@@ -2,7 +2,7 @@ const POOLS = [
   {
     name: 'MATICx',
     chain: 'polygon',
-    underlying_tokens: ['0x0000000000000000000000000000000000001010'],
+    underlying_tokens: ['0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'],
     pool_address: '0xfa68fb4628dff1028cfec22b4162fccd0d45efb6',
     investing_address: '0xfd225c9e6601c9d38d8f98d8731bf59efcf8c0e3',
     staking_address: null,
