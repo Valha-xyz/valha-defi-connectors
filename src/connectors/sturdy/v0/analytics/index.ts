1b210854dee6d3af5ae7bc9dff05f623a3653f12
octavionotpunk <octave.mesnard@gmail.com>
2023-09-18 19:55:55 +0200

    add sturdy tvl and apy historical

--- src/connectors/sturdy/v0/analytics/index.ts
+++ src/connectors/sturdy/v0/analytics/index.ts
@@ -3,6 +3,12 @@ import {
   type AnalyticsExport,
 } from '../../../../utils/types/connector-types';
 import { getVaults } from '../pools/updatePools';
+import pools from '../pools/pools';
+import { ethers } from 'ethers';
+import { getNodeProvider } from '../../../../utils/getNodeProvider';
+import { ROUTERABI } from '../abi/Router';
+import { POOLABI } from '../abi/Pool';
+import { getGeckoTokenPrice } from '../../../../utils/prices/getGeckoTokenPrice'
 
 /* eslint-disable @typescript-eslint/no-var-requires */
 const _ = require('lodash');
@@ -12,15 +18,36 @@ async function analytics(
   poolAddress,
 ): Promise<Analytics | Record<never, never>> {
   try {
+
+    const POOLS2 = await pools()
+    if (!POOLS2 || POOLS2.length === 0) return {}
+    const currentPool2 = POOLS2.find((pool) => pool.pool_address == poolAddress);
+
+
     const POOLS = await getVaults(chain);
     const currentPool = POOLS.find((pool) => pool.address == poolAddress);
 
+    //  price token
+
+    const { data, err } = await getGeckoTokenPrice(chain,currentPool2.underlying_tokens[0]);
+
+    const provider = getNodeProvider(chain)
+    if (!provider) throw new Error('No provider was found.')
+    const POOL = new ethers.Contract(currentPool2.investing_address,ROUTERABI,provider);
+
+    const generalData = await POOL.getReserveData(currentPool2.underlying_tokens[0]);
+    const formattedApy =  generalData[2] / 10 ** 27;
+
+    const TOKEN = new ethers.Contract(poolAddress,POOLABI,provider);
+    const tvl = data * TOKEN.totalSupply();
+
+
     if (!POOLS || POOLS.length === 0) return {};
 
-    const tvl = currentPool.tvl;
+    // const tvl = currentPool.tvl;
     const outloans = currentPool.totalBorrowUsd;
     const liquidity = tvl - outloans;
-    const apy = currentPool.base * 100;
+    // const apy = currentPool.base * 100;
 
     const result = {
       status: currentPool.active,
@@ -29,16 +56,17 @@ async function analytics(
       outloans,
       losses: null,
       capacity: liquidity,
-      apy,
-      activity_apy: apy,
+      apy: formattedApy,
+      activity_apy: formattedApy,
       rewards_apy: 0, // No rewards on deposit pools
       boosting_apy: 0,
       share_price: 1,
       minimum_deposit: null,
       maximum_deposit: null,
     };
-
+    console.log(result);
     return result;
+    
   } catch (err) {
     console.log(err);
     return null;
